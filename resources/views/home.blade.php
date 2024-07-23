d9e441fbf3bb90bdf5c1cb404ff54ec473d32fe8
Humam Ibadillah <humamifakhri@gmail.com>
2024-07-23 09:42:08 +0700

    Membuat section Footer [Desktop]

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -58,7 +58,7 @@
             </div>
             <img src="img/wave.png" alt="" class="mt-5 img-fluid w-full rotate-180 translate-y-1">
         </section>
-        <section class="pt-10 relative">
+        <section class="pt-10 relative mb-20">
             <div class="absolute left-0 top-3 translate-y-36 w-full">
                 <hr class="border-zinc-500">
             </div>
@@ -73,4 +73,24 @@
             </div>
         </section>
     </main>
+    <footer class="pt-8 pb-2 max-container padding-container">
+        <div class="flex gap-4 items-end justify-end border-y-2 border-gray-200 py-8">
+            <p class="font-semibold text-2xl text-gray-200 text-end">Let's make a party,<br>and together, we can conquer
+                the world.</p>
+            <button
+                class="py-5 px-10 bg-primary rounded-lg border-2 border-black shadow-[-6px_6px_rgba(0,0,0,1)] hover:shadow-none hover:-translate-x-1 hover:translate-y-1 transition"><a
+                    href="#" class="font-bold text-2xl text-white">Let's make a deal!</a></button>
+            <button
+                class="pb-2 px-3 bg-[#00ccff] rounded-lg border-2 border-black shadow-[-6px_6px_rgba(0,0,0,1)] hover:shadow-none hover:-translate-x-1 hover:translate-y-1 transition"><a
+                    href="#" class="font-bold text-2xl text-white text-center">
+                    <small class="text-[10px] text-white">Skipped the content?</small>
+                    <div class="flex items-center justify-center gap-1 -mt-2">
+                      <p class="text-white">Goin up</p>
+                      <i class="text-white fa-solid fa-arrow-up-long"></i>
+                    </div>
+                </a>
+            </button>
+        </div>
+        <p class="text-end text-xs text-gray-200 mt-2">COPYRIGHT ©2024<br>BVCKLESMIGGLE, ALL RIGHT RESERVED</p>
+    </footer>
 </x-layout>
