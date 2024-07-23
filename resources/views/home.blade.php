c39a73d1dce579ebc607295b0897a8de674b71eb
Humam Ibadillah <humamifakhri@gmail.com>
2024-07-23 09:19:25 +0700

    Membuat section Equipments [Desktop]

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,12 @@
     <main class="mb-36">
         <section class="max-container padding-container">
             <img src="/img/banner.png" alt="" class="img-fluid w-full my-4 border-2 border-gray-200">
-            <h1 class="font-bold text-gray-200 text-6xl"><span class="color-primary">></span>SHOUT OUT<span
-                    class="color-primary">__</span></h1>
+            <h1 class="font-bold text-gray-200 text-5xl"><span class="color-primary">></span>SHOUT OUT<span
+                    class="color-primary">_</span></h1>
             <div class="grid grid-cols-3 gap-4">
                 <div class="col-span-2 gap-4">
                     <div class="flex justify-between items-end">
-                        <p class="font-semibold text-2xl text-gray-200 max-w-[30%]">We are a<br>creative-smith<br>at the
+                        <p class="font-semibold text-2xl text-gray-200 max-w-[50%]">We are a<br>creative-smith<br>at the
                             intersection<br>of craft, community,<br>music, and lifestyle.</p>
                         <div class="flex justify-end ms-20 me-5 flex-grow">
                             <img src="/img/home.png" alt="" class="max-w-[70%] h-auto">
@@ -17,7 +17,8 @@
                     </div>
                 </div>
                 <div class="flex flex-col items-end justify-end gap-10">
-                    <p class="text-end text-lg text-gray-200">Our goal is to deliver amazing<br>experiences that
+                    <p class="text-light text-end text-lg text-gray-200">Our goal is to deliver amazing<br>experiences
+                        that
                         make<br>people "awake" with<br>encourage their<br>deepest desire<br>at lifestyle,<br>arts,
                         and<br>culture.</p>
                     <img src="/img/v.png" alt="" class="img-fluid">
@@ -32,13 +33,30 @@
                         MUSIC, <br>AND OTHER <br>SINS<span class="color-primary">__</span></h1>
                     <p class="font-semibold text-2xl text-gray-200 text-end">Keep update<br>and
                         still<br>rockNroll<br>with our<br>news and<br>field report.</p>
-                    <button class="block ms-auto mt-24 py-5 px-10 bg-primary rounded-lg border-2 border-black shadow-[-6px_6px_rgba(0,0,0,1)] hover:shadow-none hover:-translate-x-1 hover:translate-y-1 transition"><a
+                    <button
+                        class="block ms-auto mt-24 py-5 px-10 bg-primary rounded-lg border-2 border-black shadow-[-6px_6px_rgba(0,0,0,1)] hover:shadow-none hover:-translate-x-1 hover:translate-y-1 transition"><a
                             href="#" class="font-bold text-2xl text-white">Check the hype!</a></button>
                 </div>
             </div>
             <div class="absolute left-0 bottom-0 -translate-y-24 w-full -z-10">
-              <hr class="border-zinc-500">
+                <hr class="border-zinc-500">
             </div>
+        </section>
+        <section class="mt-5 bg-white">
+            <img src="img/wave.png" alt="" class="img-fluid w-full">
+            <div class="flex justify-between items-center gap-4 max-container padding-container">
+                <div>
+                    <h1 class="font-bold text-end text-gray-950 text-5xl"><span class="color-primary">></span>STEP
+                        UP<br>YOUR<br>EQUIPMENTS<span class="color-primary">_</span></h1>
+                    <button
+                        class="block ms-auto mt-10 py-5 px-10 bg-primary rounded-lg border-2 border-black shadow-[-6px_6px_rgba(0,0,0,1)] hover:shadow-none hover:-translate-x-1 hover:translate-y-1 transition"><a
+                            href="#" class="font-bold text-2xl text-white">Check Our Inventory!</a></button>
+                </div>
+                <img src="/img/eqp.png" alt="" class="img-fluid">
+                <p class="font-semibold text-2xl text-zinc-500 text-end">Don't
+                    let<br>aging<br>make<br>you<br>uncool.<br>Shape urself.</p>
+            </div>
+            <img src="img/wave.png" alt="" class="mt-5 img-fluid w-full rotate-180 translate-y-1">
         </section>
     </main>
 </x-layout>
